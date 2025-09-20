d48477a1a818f1a92ac3aff27d808c6440b928c6
Santiagom123 <santiagomontenegroball@gmail.com>
2025-09-20 12:41:45 -0500

    Create SockOS_desk.asm

---
+++ SockOS/SockOS_desk.asm
@@ -0,0 +1,15 @@
+
+DESKTOP FILL BLUE
+WINDOW CREATE main,5,3,50,20,"SockOS Desktop"
+WINDOW FILL main,0x003366
+
+
+DESKTOP ICON icon_shell,6,5,"Shell"
+DESKTOP ICON icon_calc,6,9,"Calc"
+DESKTOP ICON icon_notepad,6,13,"Notepad"
+
+
+BUTTON CREATE btn_exit,main,40,1,8,1,"EXIT"
+
+
+WINDOW WRITE main,2,2,"Bienvenido a SockOS"
